bf7590d51ad23cf80b43003562e3e1f9edb40907
John McCormick <jmccormick@squiz.co.uk>
2019-03-05 11:44:58 +0000

    Updating sw.js again

--- sw.js
+++ sw.js
@@ -1,30 +1,73 @@
-var CACHE_NAME = 'jonomcleish-cache-v1';
-var urlsToCache = [
-    '/index.html',
-    '/manifest.json',
-    '/normalize.css',
-    '/prefixfree.min.js',
-    '/sw.js',
-    '/images/jonomcleish-192.png',
-    '/images/jonomcleish-512.png'
-];
-
-self.addEventListener('install', function(e) {
+const cacheName = 'jonomcleish-cache-v3';
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/normalize.css',
+        '/prefixfree.min.js',
+        '/sw.js',
+        '/images/jonomcleish-192.png',
+        '/images/jonomcleish-512.png'
+      ])
+      .then(() => self.skipWaiting());
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
       return response || fetch(event.request);
     })
   );
 });
 
+
+// self.addEventListener('install', function(e) {
+//   e.waitUntil(
+//     caches.open('jonomcleish-cache-v2').then(function(cache) {
+//       return cache.addAll([
+//           '/index.html',
+//           '/manifest.json',
+//           '/normalize.css',
+//           '/prefixfree.min.js',
+//           '/sw.js',
+//           '/images/jonomcleish-192.png',
+//           '/images/jonomcleish-512.png'
+//       ]);
+//     })
+//   );
+// });
+//
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+
+// var CACHE_NAME = 'jonomcleish-cache-v1';
+// var urlsToCache = [
+//     '/index.html',
+//     '/manifest.json',
+//     '/normalize.css',
+//     '/prefixfree.min.js',
+//     '/sw.js',
+//     '/images/jonomcleish-192.png',
+//     '/images/jonomcleish-512.png'
+// ];
+//
 // self.addEventListener('install', function(event) {
 //   // Perform install steps
 //   event.waitUntil(
